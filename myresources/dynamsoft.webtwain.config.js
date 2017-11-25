c2251dfba5ed86056520b4ae355b589dec711dca
Paul Bartlett <paul.bartlett@everyonecounts.com>
2017-11-25 15:58:22 -0800

    Add resources

---
+++ myresources/dynamsoft.webtwain.config.js
@@ -0,0 +1,34 @@
+﻿//
+// Dynamsoft JavaScript Library for Basic Initiation of Dynamic Web TWAIN
+// More info on DWT: http://www.dynamsoft.com/Products/WebTWAIN_Overview.aspx
+//
+// Copyright 2017, Dynamsoft Corporation 
+// Author: Dynamsoft Team
+// Version: 13.2
+//
+/// <reference path="dynamsoft.webtwain.initiate.js" />
+var Dynamsoft = Dynamsoft || { WebTwainEnv: {} };
+
+Dynamsoft.WebTwainEnv.AutoLoad = true;
+
+///
+Dynamsoft.WebTwainEnv.Containers = [{ContainerId:'dwtcontrolContainer', Width:270, Height:350}];
+
+/// If you need to use multiple keys on the same server, you can combine keys and write like this 
+/// Dynamsoft.WebTwainEnv.ProductKey = 'key1;key2;key3';
+Dynamsoft.WebTwainEnv.ProductKey = '******';
+
+///
+Dynamsoft.WebTwainEnv.Trial = false;
+
+///
+Dynamsoft.WebTwainEnv.ActiveXInstallWithCAB = false;
+
+///
+// Dynamsoft.WebTwainEnv.ResourcesPath = 'Resources';
+
+/// All callbacks are defined in the dynamsoft.webtwain.install.js file, you can customize them.
+// Dynamsoft.WebTwainEnv.RegisterEvent('OnWebTwainReady', function(){
+// 		// webtwain has been inited
+// });
+
